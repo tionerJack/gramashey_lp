80cfd83a5b4d167c303d50bacca81fc977e1bc53
tionerJack <tioner210@gmail.com>
2023-12-31 13:24:07 -0600

    add style

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'dart:html' as html;
@@ -5,5 +7,5 @@ import 'app/index.dart';
 
 Future<void> main() async {
   runApp(const MyApp());
-  html.document.getElementById('splash')!.style.display = 'none';
+  html.document.getElementById('centered')!.remove();
 }
