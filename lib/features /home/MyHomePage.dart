ddc348771e190769dff9713d28947ef512f91ebe
tionerJack <tioner210@gmail.com>
2024-01-03 15:33:34 -0600

    add animated float

--- lib/features /home/MyHomePage.dart
+++ lib/features /home/MyHomePage.dart
@@ -47,59 +47,62 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        controller: _scrollController,
-        padding: EdgeInsets.symmetric(
-            horizontal: MediaQuery.of(context).size.height * .02),
-        child: Consumer(
-            builder: (BuildContext context, WidgetRef ref, Widget? child) {
-          final benefits = ref.watch(benefitsProvider);
+        body: SingleChildScrollView(
+          controller: _scrollController,
+          padding: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.height * .02),
+          child: Consumer(
+              builder: (BuildContext context, WidgetRef ref, Widget? child) {
+            final benefits = ref.watch(benefitsProvider);
 
-          return Column(
-            children: [
-              SizedBox(height: MediaQuery.of(context).size.height * .03),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Hero(
-                      tag: 'hero-tag-img15',
-                      child: Image.asset(
-                        "assets/img15.png",
-                        fit: BoxFit.contain,
-                        height: MediaQuery.of(context).size.height * .5,
-                        width: MediaQuery.of(context).size.width * .85,
-                      ))
-                ],
-              ),
-              SizedBox(height: MediaQuery.of(context).size.height * .03),
-              const Divider(),
-              ...benefits
-                  .map((e) => BenefitListTile(
-                        benefit: e,
-                        height: MediaQuery.of(context).size.height * .1,
-                        leading: benefits.indexOf(e).isEven,
-                        topMargin: MediaQuery.of(context).size.height,
-                      ))
-                  .expand((element) => [element, Divider()])
-                  .toList(),
-              SizedBox(height: MediaQuery.of(context).size.height * .15),
-            ],
-          );
-        }),
-      ),
-      floatingActionButton: isAtBottom
-          ? FloatingActionButton.extended(
-              onPressed: _launchWhats,
-              label: const Text('Mas información'),
-              icon: const Icon(FontAwesomeIcons.whatsapp),
-              backgroundColor: Colors.green.shade800,
-            )
-          : FloatingActionButton(
-              onPressed: _launchWhats,
-              backgroundColor: Colors.green.shade800,
-              child: const Icon(FontAwesomeIcons.whatsapp),
-            ),
-    );
+            return Column(
+              children: [
+                SizedBox(height: MediaQuery.of(context).size.height * .03),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Hero(
+                        tag: 'hero-tag-img15',
+                        child: Image.asset(
+                          "assets/img15.png",
+                          fit: BoxFit.contain,
+                          height: MediaQuery.of(context).size.height * .5,
+                          width: MediaQuery.of(context).size.width * .85,
+                        ))
+                  ],
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * .03),
+                const Divider(),
+                ...benefits
+                    .map((e) => BenefitListTile(
+                          benefit: e,
+                          height: MediaQuery.of(context).size.height * .1,
+                          leading: benefits.indexOf(e).isEven,
+                          topMargin: MediaQuery.of(context).size.height,
+                        ))
+                    .expand((element) => [element, Divider()])
+                    .toList(),
+                SizedBox(height: MediaQuery.of(context).size.height * .15),
+              ],
+            );
+          }),
+        ),
+        floatingActionButton: AnimatedSwitcher(
+          duration: Duration(seconds: 5),
+          switchInCurve: Curves.bounceInOut,
+          child: isAtBottom
+              ? FloatingActionButton.extended(
+                  onPressed: _launchWhats,
+                  label: const Text('Mas información'),
+                  icon: const Icon(FontAwesomeIcons.whatsapp),
+                  backgroundColor: Colors.green.shade800,
+                )
+              : FloatingActionButton(
+                  onPressed: _launchWhats,
+                  backgroundColor: Colors.green.shade800,
+                  child: const Icon(FontAwesomeIcons.whatsapp),
+                ),
+        ));
   }
 
   void _launchWhats() async {
